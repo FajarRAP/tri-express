a7818f300342de30a06f62bd0cde317c4cf9da4a
Fajar Riansyah <fajary781@gmail.com>
2025-08-13 11:48:26 +0700

    feat: add colors

---
+++ lib/core/themes/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const black = Color(0xFF212329);
+const border = Color(0xFFEAEFF5);
+const danger = Color(0xFFE71B1B);
+const gray = Color(0xFFBBBBBB);
+const graySecondary = Color(0xFFCCCCCC);
+const grayTertiary = Color(0xFF767676);
+const light = Color(0xFFFFFFFF);
+const lightBlue = Color(0xFFECEFFF);
+const primary = Color(0xFF0E2BCD);
+const secondary = Color(0xFF25C0DE);
+const successEmphasis = Color(0xFFE5FFEA);
+const text = Color(0xFF152B52);
+const warning = Color(0xFFD7974D);
+const warningEmphasis = Color(0xFFEDDC3F);
+const whiteSecondary = Color(0xFFEFF1F3);
+const whiteTertiary = Color(0xFFF6F6F6);
