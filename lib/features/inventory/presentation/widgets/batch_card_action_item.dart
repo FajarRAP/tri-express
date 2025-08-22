3434e181dd436c80d7f20fb058c84bfc5b36c596
Fajar Riansyah <fajary781@gmail.com>
2025-08-22 15:43:07 +0700

    feat: add card action item widget

---
+++ lib/features/inventory/presentation/widgets/batch_card_action_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/themes/colors.dart';
+import '../pages/on_the_way_page.dart';
+import 'batch_card_item.dart';
+
+class BatchCardActionItem extends StatelessWidget {
+  const BatchCardActionItem({
+    super.key,
+    required this.batch,
+  });
+
+  final Batch batch;
+
+  @override
+  Widget build(BuildContext context) {
+    return BatchCardItem(
+      batch: batch,
+      action: PopupMenuButton(
+        itemBuilder: (context) => <PopupMenuEntry>[
+          PopupMenuItem(
+            onTap: () => debugPrint('Deleted ${batch.id}'),
+            value: 'action_1',
+            child: Text(
+              'Hapus Batch',
+              style: const TextStyle(
+                color: danger,
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+        color: light,
+        icon: const Icon(Icons.more_horiz_outlined),
+        menuPadding: EdgeInsets.zero,
+        padding: EdgeInsets.zero,
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+}
