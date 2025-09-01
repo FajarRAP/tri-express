0ba8f05be7e851193e9102ee3a3b8315a63455b8
Fajar Riansyah <fajary781@gmail.com>
2025-09-01 14:01:11 +0700

    feat (router): register received goods page to router

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -5,7 +5,8 @@ import '../../features/inventory/presentation/pages/batch_detail_page.dart';
 import '../../features/inventory/presentation/pages/inventory_page.dart';
 import '../../features/inventory/presentation/pages/item_detail_page.dart';
 import '../../features/inventory/presentation/pages/on_the_way_page.dart';
-import '../../features/inventory/presentation/pages/receive_goods_page.dart';
+import '../../features/inventory/presentation/pages/prepare_goods_page.dart';
+import '../../features/inventory/presentation/pages/received_goods_page.dart';
 import '../../features/inventory/presentation/pages/setting_page.dart';
 import '../pages/home_page.dart';
 import '../utils/constants.dart';
@@ -15,7 +16,9 @@ final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
 final _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 const menuRoute = '/menu';
+const receivedGoodsRoute = '$menuRoute/received-goods';
 const receiveGoodsRoute = '$menuRoute/receive-goods';
+const prepareGoodsRoute = '$menuRoute/prepare-goods';
 
 const onTheWayRoute = '/on-the-way';
 const batchDetailRoute = '/batch-detail';
@@ -46,8 +49,12 @@ final router = GoRouter(
           builder: (context, state) => const HomePage(),
           routes: <RouteBase>[
             GoRoute(
-              path: 'receive-goods',
-              builder: (context, state) => const ReceiveGoodsPage(),
+              path: 'received-goods',
+              builder: (context, state) => const ReceivedGoodsPage(),
+            ),
+            GoRoute(
+              path: 'prepare-goods',
+              builder: (context, state) => const PrepareGoodsPage(),
             ),
           ],
         ),
