2328c777e9ae5407d5f84309ff503b8d4f9c6f68
Fajar Riansyah <fajary781@gmail.com>
2025-09-02 15:40:28 +0700

    feat (base card): allow customizable background and border colors

--- lib/core/widgets/base_card.dart
+++ lib/core/widgets/base_card.dart
@@ -5,18 +5,22 @@ import '../themes/colors.dart';
 class BaseCard extends StatelessWidget {
   const BaseCard({
     super.key,
+    this.backgroundColor,
+    this.borderColor,
     required this.child,
   });
 
+  final Color? backgroundColor;
+  final Color? borderColor;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color: graySecondary),
+        border: Border.all(color: borderColor ?? graySecondary),
         borderRadius: BorderRadius.circular(10),
-        color: light,
+        color: backgroundColor ?? light,
       ),
       padding: const EdgeInsets.all(20),
       child: child,
