88c065d7ca89b12078abb4dfc4ba87749e318433
Fajar Riansyah <fajary781@gmail.com>
2025-09-04 10:49:57 +0700

    feat (core): implement Exception handling in Failure class

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/foundation.dart';
 
-class Failure {
+class Failure implements Exception {
   const Failure({
-    required String? message,
-    this.statusCode = 500,
-  }) : message = (kDebugMode ? message : null) ?? 'Terjadi Kesalahan';
+    String? message,
+    int? statusCode,
+  })  : message = (kDebugMode ? message : null) ?? 'Terjadi Kesalahan',
+        statusCode = statusCode ?? 500;
+
   final String message;
   final int statusCode;
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({
+    String? message,
+    super.statusCode,
+  }) : super(
+            message:
+                (kDebugMode ? message : null) ?? 'Terjadi Kesalahan Server');
 }
