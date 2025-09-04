17c906734b0a974b75331b60a60ca386cf767b65
Fajar Riansyah <fajary781@gmail.com>
2025-09-04 15:04:54 +0700

    feat (auth data sources): add auth local data sources with the tests

---
+++ lib/features/auth/data/data_sources/auth_local_data_sources.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../../../../core/exceptions/cache_exception.dart';
+import '../../../../core/utils/constants.dart';
+
+abstract class AuthLocalDataSources {
+  Future<String?> getAccessToken();
+  Future<String?> getRefreshToken();
+  Future<void> cacheToken({required String accessToken, String? refreshToken});
+  Future<void> clearToken();
+}
+
+class AuthLocalDataSourcesImpl extends AuthLocalDataSources {
+  AuthLocalDataSourcesImpl({required this.storage});
+
+  final FlutterSecureStorage storage;
+
+  @override
+  Future<void> cacheToken(
+      {required String accessToken, String? refreshToken}) async {
+    try {
+      await storage.write(key: accessTokenKey, value: accessToken);
+      await storage.write(key: refreshTokenKey, value: refreshToken);
+    } catch (e) {
+      throw CacheException(message: '$e');
+    }
+  }
+
+  @override
+  Future<void> clearToken() async {
+    try {
+      await storage.deleteAll();
+    } catch (e) {
+      throw CacheException(message: '$e');
+    }
+  }
+
+  @override
+  Future<String?> getAccessToken() async {
+    try {
+      return await storage.read(key: accessTokenKey);
+    } catch (e) {
+      throw CacheException(message: '$e');
+    }
+  }
+
+  @override
+  Future<String?> getRefreshToken() async {
+    try {
+      return await storage.read(key: refreshTokenKey);
+    } catch (e) {
+      throw CacheException(message: '$e');
+    }
+  }
+}
