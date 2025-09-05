9573fd989f27805797a18f4b7f83bdc2ac7650a2
Fajar Riansyah <fajary781@gmail.com>
2025-09-05 19:00:24 +0700

    feat (home page): implement fetch current user, summary, and banner

--- lib/features/core/presentation/pages/home_page.dart
+++ lib/features/core/presentation/pages/home_page.dart
@@ -1,247 +1,215 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/routes/router.dart';
 import '../../../../core/themes/colors.dart';
+import '../../../../core/utils/constants.dart';
+import '../../../../core/utils/helpers.dart';
 import '../../../../core/widgets/notification_icon_button.dart';
-import '../../../../core/widgets/primary_icon_rectangle.dart';
+import '../../../auth/presentation/cubit/auth_cubit.dart';
+import '../cubit/core_cubit.dart';
+import '../widgets/action_card.dart';
+import '../widgets/data_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authCubit = context.read<AuthCubit>();
+    final coreCubit = context.read<CoreCubit>();
+
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: <Widget>[
-              const CircleAvatar(radius: 24),
-              const SizedBox(width: 12),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
+      child: BlocBuilder<AuthCubit, AuthState>(
+        bloc: authCubit..fetchCurrentUser(),
+        builder: (context, state) {
+          if (state is FetchCurrentUserLoading) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+
+          if (state is FetchCurrentUserLoaded) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 24,
+                      foregroundImage: state.user.avatarUrl != null
+                          ? NetworkImage(state.user.avatarUrl!)
+                          : null,
+                    ),
+                    const SizedBox(width: 12),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          state.user.name,
+                          style: const TextStyle(
+                            color: black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        if (!state.user.roles.contains(superAdminRole))
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              const Icon(
+                                Icons.location_pin,
+                                color: grayTertiary,
+                                size: 14,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(
+                                'Warehouse Location',
+                                style: const TextStyle(
+                                  color: grayTertiary,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          )
+                      ],
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  const NotificationIconButton(),
+                  const SizedBox(width: 16),
+                ],
+              ),
+              body: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
                 children: <Widget>[
-                  Text(
-                    'User Name',
-                    style: const TextStyle(
-                      color: black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
+                  const SizedBox(height: 24),
+                  BlocConsumer<CoreCubit, CoreState>(
+                    bloc: coreCubit..fetchSummary(),
+                    buildWhen: (previous, current) => current is FetchSummary,
+                    listener: (context, state) {
+                      if (state is FetchSummaryError) {
+                        TopSnackbar.dangerSnackbar(message: state.message);
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is FetchSummaryLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator.adaptive(),
+                        );
+                      }
+
+                      if (state is FetchSummaryLoaded) {
+                        return Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: DataCard(
+                                icon: Icons.local_shipping_outlined,
+                                label: 'On the way',
+                                number: state.summary[0],
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: DataCard(
+                                icon: Icons.inventory_2_outlined,
+                                label: 'Koli diterima',
+                                number: state.summary[1],
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: DataCard(
+                                icon: Icons.send_outlined,
+                                label: 'Koli terkirim',
+                                number: state.summary[2],
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+
+                      return const SizedBox();
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    height: 180,
+                    child: BlocBuilder<CoreCubit, CoreState>(
+                      bloc: coreCubit..fetchBanners(),
+                      buildWhen: (previous, current) => current is FetchBanners,
+                      builder: (context, state) {
+                        if (state is FetchBannersLoading) {
+                          return const Center(
+                            child: CircularProgressIndicator.adaptive(),
+                          );
+                        }
+
+                        if (state is FetchBannersLoaded) {
+                          return CarouselView.weighted(
+                            enableSplash: false,
+                            flexWeights: [1],
+                            itemSnapping: true,
+                            children: state.banners
+                                .map((banner) =>
+                                    Image.network(banner, fit: BoxFit.cover))
+                                .toList(),
+                          );
+                        }
+
+                        return const SizedBox();
+                      },
                     ),
                   ),
-                  const SizedBox(height: 4),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Silakan Tentukan Apa yang Ingin Anda Lakukan',
+                    style: TextStyle(
+                      color: black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
                     children: <Widget>[
-                      const Icon(
-                        Icons.location_pin,
-                        color: grayTertiary,
-                        size: 14,
+                      ActionCard(
+                        onTap: () => context.push(receivedGoodsRoute),
+                        icon: Icons.inventory_2_outlined,
+                        title: 'Terima Barang',
                       ),
-                      const SizedBox(width: 6),
-                      Text(
-                        'Warehouse Location',
-                        style: const TextStyle(
-                          color: grayTertiary,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                        ),
+                      ActionCard(
+                        onTap: () => context.push(prepareGoodsRoute),
+                        icon: Icons.inventory_2_outlined,
+                        title: 'Persiapan Barang',
+                      ),
+                      ActionCard(
+                        onTap: () => context.push(sendGoodsRoute),
+                        icon: Icons.inventory_2_outlined,
+                        title: 'Kirim Barang',
+                      ),
+                      ActionCard(
+                        icon: Icons.inventory_2_outlined,
+                        title: 'Ambil di Gudang',
                       ),
                     ],
                   ),
                 ],
               ),
-            ],
-          ),
-          actions: <Widget>[
-            const NotificationIconButton(),
-            const SizedBox(width: 16),
-          ],
-        ),
-        body: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          children: <Widget>[
-            const SizedBox(height: 24),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: const _DataCard(
-                    icon: Icons.local_shipping_outlined,
-                    label: 'On the way',
-                    number: 100,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: const _DataCard(
-                    icon: Icons.inventory_2_outlined,
-                    label: 'Koli diterima',
-                    number: 30,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: const _DataCard(
-                    icon: Icons.send_outlined,
-                    label: 'Koli terkirim',
-                    number: 150,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Silakan Tentukan Apa yang Ingin Anda Lakukan',
-              style: TextStyle(
-                color: black,
-                fontSize: 20,
-                fontWeight: FontWeight.w800,
-              ),
-            ),
-            const SizedBox(height: 16),
-            GridView.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              children: <Widget>[
-                _ActionCard(
-                  onTap: () => context.push(receivedGoodsRoute),
-                  icon: Icons.inventory_2_outlined,
-                  title: 'Terima Barang',
-                ),
-                _ActionCard(
-                  onTap: () => context.push(prepareGoodsRoute),
-                  icon: Icons.inventory_2_outlined,
-                  title: 'Persiapan Barang',
-                ),
-                _ActionCard(
-                  onTap: () => context.push(sendGoodsRoute),
-                  icon: Icons.inventory_2_outlined,
-                  title: 'Kirim Barang',
-                ),
-                _ActionCard(
-                  icon: Icons.inventory_2_outlined,
-                  title: 'Ambil di Gudang',
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+            );
+          }
 
-class _ActionCard extends StatelessWidget {
-  const _ActionCard({
-    this.onTap,
-    required this.icon,
-    required this.title,
-  });
-
-  final void Function()? onTap;
-  final IconData icon;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: graySecondary),
-          borderRadius: BorderRadius.circular(30),
-          color: light,
-        ),
-        padding: const EdgeInsets.all(20),
-        height: 150,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              title,
-              style: const TextStyle(
-                color: black,
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const SizedBox(height: 10),
-            PrimaryIconRectangle(icon: icon),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _DataCard extends StatelessWidget {
-  const _DataCard({
-    required this.number,
-    required this.icon,
-    required this.label,
-  });
-
-  final int number;
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: lightBlue),
-        borderRadius: BorderRadius.circular(14),
-        color: light,
-      ),
-      height: 80,
-      padding: const EdgeInsets.all(14),
-      child: Column(
-        children: <Widget>[
-          Text(
-            '$number',
-            style: TextStyle(
-              color: black,
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Text(
-            label,
-            style: TextStyle(
-              color: grayTertiary,
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-            ),
-            overflow: TextOverflow.ellipsis,
-          ),
-          // Row(
-          //   children: <Widget>[
-          //     Row(
-          //       crossAxisAlignment: CrossAxisAlignment.center,
-          //       children: <Widget>[
-          //         Icon(
-          //           icon,
-          //           color: grayTertiary,
-          //           size: 16,
-          //         ),
-          //         const SizedBox(width: 6),
-          //         Text(
-          //           label,
-          //           style: TextStyle(
-          //             color: grayTertiary,
-          //             fontSize: 14,
-          //             fontWeight: FontWeight.w400,
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ],
-          // ),
-        ],
+          return const SizedBox();
+        },
       ),
     );
   }
