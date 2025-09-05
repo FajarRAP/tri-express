2e89d5b8fc8465e9dde5967c87fc23f1b73107fd
Fajar Riansyah <fajary781@gmail.com>
2025-09-05 12:03:15 +0700

    feat (service locator): register core domain dependencies

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -7,10 +7,17 @@ import 'features/auth/data/data_sources/auth_local_data_sources.dart';
 import 'features/auth/data/data_sources/auth_remote_data_sources.dart';
 import 'features/auth/data/repositories/auth_repositories_impl.dart';
 import 'features/auth/domain/repositories/auth_repositories.dart';
+import 'features/auth/domain/use_cases/complete_onboarding_use_case.dart';
 import 'features/auth/domain/use_cases/fetch_current_user_use_case.dart';
 import 'features/auth/domain/use_cases/login_use_case.dart';
 import 'features/auth/domain/use_cases/logout_use_case.dart';
 import 'features/auth/presentation/cubit/auth_cubit.dart';
+import 'features/core/data/data_sources/core_local_data_sources.dart';
+import 'features/core/data/data_sources/core_remote_data_sources.dart';
+import 'features/core/data/repositories/core_repositories_impl.dart';
+import 'features/core/domain/repositories/core_repositories.dart';
+import 'features/core/domain/use_cases/fetch_banners_use_case.dart';
+import 'features/core/presentation/cubit/core_cubit.dart';
 
 final getIt = GetIt.instance;
 
@@ -31,11 +38,26 @@ void setupServiceLocator() {
     )..interceptors.add(CustomInterceptor()),
   );
 
+  // Core
   getIt
-    ..registerLazySingleton<AuthRemoteDataSources>(
-        () => AuthRemoteDataSourcesImpl(dio: getIt.get()))
+    ..registerLazySingleton<CoreLocalDataSources>(
+        () => CoreLocalDataSourcesImpl(storage: getIt.get()))
+    ..registerLazySingleton<CoreRemoteDataSources>(
+        () => CoreRemoteDataSourcesImpl(dio: getIt.get()))
+    ..registerLazySingleton<CoreRepositories>(() => CoreRepositoriesImpl(
+        coreLocalDataSources: getIt.get(), coreRemoteDataSources: getIt.get()))
+    ..registerLazySingleton<CoreCubit>(() => CoreCubit(
+        completeOnboardingUseCase:
+            CompleteOnboardingUseCase(authRepositories: getIt.get()),
+        fetchBannersUseCase:
+            FetchBannersUseCase(coreRepositories: getIt.get())));
+
+  // Auth
+  getIt
     ..registerLazySingleton<AuthLocalDataSources>(
         () => AuthLocalDataSourcesImpl(storage: getIt.get()))
+    ..registerLazySingleton<AuthRemoteDataSources>(
+        () => AuthRemoteDataSourcesImpl(dio: getIt.get()))
     ..registerLazySingleton<AuthRepositories>(() => AuthRepositoriesImpl(
         authLocalDataSources: getIt.get(), authRemoteDataSources: getIt.get()))
     ..registerLazySingleton<AuthCubit>(() => AuthCubit(
