c366311cffe0bac50ecd62732fb5c2525f486a1c
Fajar Riansyah <fajary781@gmail.com>
2025-09-05 17:57:44 +0700

    fix (main): refactor main function to initialize storage and auth data sources

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'core/routes/router.dart';
 import 'core/themes/theme.dart';
+import 'core/utils/constants.dart';
 import 'core/utils/helpers.dart';
+import 'features/auth/data/data_sources/auth_local_data_sources.dart';
 import 'features/auth/presentation/cubit/auth_cubit.dart';
+import 'features/core/presentation/cubit/core_cubit.dart';
 import 'service_locator.dart';
 
 late Faker faker;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
   faker = Faker();
+  final storage = getIt.get<FlutterSecureStorage>();
+  final authLocal = getIt.get<AuthLocalDataSources>();
+  initialLocation = await storage.read(key: onboardingKey) == null
+      ? onboardingRoute
+      : await authLocal.getAccessToken() != null
+          ? menuRoute
+          : loginRoute;
+
   runApp(const MyApp());
 }
 
@@ -23,6 +36,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider<CoreCubit>(create: (context) => getIt.get<CoreCubit>()),
         BlocProvider<AuthCubit>(create: (context) => getIt.get<AuthCubit>()),
       ],
       child: MaterialApp.router(
