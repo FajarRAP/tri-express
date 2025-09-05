6c9d19717ab1d2a7d788f25633157116c1f626d5
Fajar Riansyah <fajary781@gmail.com>
2025-09-05 17:46:32 +0700

    feat (login page): implement login functionality with form validation and state management

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/fonts/fonts.dart';
 import '../../../../core/routes/router.dart';
 import '../../../../core/themes/colors.dart';
 import '../../../../core/utils/constants.dart';
+import '../../../../core/utils/helpers.dart';
+import '../../../../core/utils/validators.dart';
 import '../../../../core/widgets/buttons/primary_button.dart';
+import '../cubit/auth_cubit.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -15,15 +19,19 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late final AuthCubit _authCubit;
   late final TextEditingController _emailController;
   late final TextEditingController _passwordController;
+  late final GlobalKey<FormState> _formKey;
   var _isObscure = true;
 
   @override
   void initState() {
     super.initState();
+    _authCubit = context.read<AuthCubit>();
     _emailController = TextEditingController();
     _passwordController = TextEditingController();
+    _formKey = GlobalKey<FormState>();
   }
 
   @override
@@ -37,99 +45,149 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 0,
-              bottom: 0,
-              child: Image.asset(
-                onboardingImagePath,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Positioned(
-              top: 24,
-              left: 16,
-              child: Image.asset(
-                logoTextImagePath,
-                width: 128,
-              ),
-            ),
-            Positioned(
-              left: 0,
-              bottom: 0,
-              right: 0,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  bottom: MediaQuery.viewInsetsOf(context).bottom,
+        child: Form(
+          key: _formKey,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 0,
+                bottom: 0,
+                child: Image.asset(
+                  loginImagePath,
+                  fit: BoxFit.cover,
                 ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(30),
+              ),
+              Positioned(
+                top: 24,
+                left: 16,
+                child: Image.asset(
+                  logoTextImagePath,
+                  width: 128,
+                ),
+              ),
+              Positioned(
+                left: 0,
+                bottom: 0,
+                right: 0,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: MediaQuery.viewInsetsOf(context).bottom,
                   ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: light,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(30),
                     ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 40,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          'Let\'s Login with Your Account',
-                          style: heading5[bold],
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 24),
-                        TextFormField(
-                          controller: _emailController,
-                          decoration: const InputDecoration(
-                            hintText: 'Masukkan email anda',
-                            labelText: 'Email',
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: light,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 40,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            'Let\'s Login with Your Account',
+                            style: heading5[bold],
+                            textAlign: TextAlign.center,
                           ),
-                        ),
-                        const SizedBox(height: 24),
-                        TextFormField(
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            hintText: 'Masukkan password anda',
-                            labelText: 'Password',
-                            suffixIcon: IconButton(
-                              onPressed: () =>
-                                  setState(() => _isObscure = !_isObscure),
-                              icon: Icon(_isObscure
-                                  ? Icons.visibility_outlined
-                                  : Icons.visibility_off_outlined),
+                          const SizedBox(height: 24),
+                          TextFormField(
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              hintText: 'Masukkan email anda',
+                              labelText: 'Email',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            textInputAction: TextInputAction.next,
+                            validator: nullValidator,
+                          ),
+                          const SizedBox(height: 24),
+                          TextFormField(
+                            onFieldSubmitted: (_) => _authCubit.login(
+                                email: _emailController.text,
+                                password: _passwordController.text),
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              hintText: 'Masukkan password anda',
+                              labelText: 'Password',
+                              suffixIcon: IconButton(
+                                onPressed: () =>
+                                    setState(() => _isObscure = !_isObscure),
+                                icon: Icon(_isObscure
+                                    ? Icons.visibility_outlined
+                                    : Icons.visibility_off_outlined),
+                              ),
+                            ),
+                            obscureText: _isObscure,
+                            keyboardType: TextInputType.visiblePassword,
+                            textInputAction: TextInputAction.send,
+                            validator: nullValidator,
+                          ),
+                          const SizedBox(height: 24),
+                          SizedBox(
+                            width: double.infinity,
+                            child: BlocConsumer<AuthCubit, AuthState>(
+                              buildWhen: (previous, current) =>
+                                  current is Login,
+                              listenWhen: (previous, current) =>
+                                  current is Login,
+                              listener: (context, state) {
+                                if (state is LoginLoaded) {
+                                  TopSnackbar.successSnackbar(
+                                      message: 'success login');
+                                  context.go(menuRoute);
+                                }
+                                if (state is LoginError) {
+                                  TopSnackbar.dangerSnackbar(
+                                      message: state.message);
+                                }
+                              },
+                              builder: (context, state) {
+                                if (state is LoginLoading) {
+                                  return const PrimaryButton(
+                                    child: Text('Masuk'),
+                                  );
+                                }
+
+                                return PrimaryButton(
+                                  onPressed: () {
+                                    if (!_formKey.currentState!.validate()) {
+                                      return;
+                                    }
+
+                                    _authCubit.login(
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                    );
+                                  },
+                                  child: const Text('Masuk'),
+                                );
+                              },
                             ),
                           ),
-                          obscureText: _isObscure,
-                        ),
-                        const SizedBox(height: 24),
-                        SizedBox(
-                          width: double.infinity,
-                          child: PrimaryButton(
-                            onPressed: () => context.go(menuRoute),
-                            child: const Text('Mulai Sekarang'),
+                          const SizedBox(height: 24),
+                          const Text(
+                            '2025 © PT. Tri Perkasa Express. All Right Reserved',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: grayTertiary,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 24),
-                        const Text(
-                          '2025 © PT. Tri Perkasa Express. All Right Reserved',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: grayTertiary,
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       resizeToAvoidBottomInset: false,
