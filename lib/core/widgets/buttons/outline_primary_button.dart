ea562b5ee6a0732823c060fbcffb0e9c8f689067
Fajar Riansyah <fajary781@gmail.com>
2025-09-10 17:04:37 +0700

    feat (outline primary button): update to support icon display

--- lib/core/widgets/buttons/outline_primary_button.dart
+++ lib/core/widgets/buttons/outline_primary_button.dart
@@ -6,23 +6,29 @@ class OutlinePrimaryButton extends StatelessWidget {
   const OutlinePrimaryButton({
     super.key,
     this.onPressed,
+    this.icon,
     required this.child,
   });
 
   final void Function()? onPressed;
+  final Widget? icon;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return ElevatedButton.icon(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         elevation: 0,
         backgroundColor: light,
         foregroundColor: primary,
+        overlayColor: primary,
+        surfaceTintColor: primary,
+        shadowColor: Colors.transparent,
         side: onPressed != null ? const BorderSide(color: primary) : null,
       ),
-      child: child,
+      icon: icon,
+      label: child,
     );
   }
 }
