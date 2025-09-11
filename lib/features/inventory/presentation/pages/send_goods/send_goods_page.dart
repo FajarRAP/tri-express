fea25460eff4e3053ea6341315a4f749339488a3
Fajar Riansyah <fajary781@gmail.com>
2025-09-11 16:51:20 +0700

    feat (send goods): refactor SendGoodsFilterPage to use stateful widget and improve input handling; add SendGoodsScanPage for scanning functionality

--- lib/features/inventory/presentation/pages/send_goods/send_goods_page.dart
+++ lib/features/inventory/presentation/pages/send_goods/send_goods_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../../core/routes/router.dart';
@@ -6,53 +7,31 @@ import '../../../../../core/themes/colors.dart';
 import '../../../../../core/utils/constants.dart';
 import '../../../../../core/widgets/decorated_icon_button.dart';
 import '../../../../../core/widgets/notification_icon_button.dart';
-import '../../widgets/batch_card_quantity_item.dart';
+import '../../cubit/inventory_cubit.dart';
+import '../../widgets/batch_card_item.dart';
+import '../../widgets/shipment_receipt_numbers_bottom_sheet.dart';
 
 class SendGoodsPage extends StatelessWidget {
   const SendGoodsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final inventoryCubit = context.read<InventoryCubit>();
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            actions: const <Widget>[
-              NotificationIconButton(),
-              SizedBox(width: 16),
-            ],
-            expandedHeight: kToolbarHeight + kSpaceBarHeight,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                alignment: Alignment.bottomCenter,
-                padding: const EdgeInsets.all(16),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Cari batch pengiriman',
-                          prefixIcon: const Icon(Icons.search_outlined),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    DecoratedIconButton(
-                      onTap: () => context.push(sendGoodsFilterRoute),
-                      icon: const Icon(Icons.add_outlined),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            floating: true,
-            snap: true,
-            pinned: true,
-            title: const Text('Kirim Barang'),
-          ),
-          if (false)
-            const SliverFillRemaining(
-              child: Padding(
+      body: BlocBuilder<InventoryCubit, InventoryState>(
+        bloc: inventoryCubit..fetchDeliveryGoods(),
+        buildWhen: (previous, current) => current is FetchDeliveryGoods,
+        builder: (context, state) {
+          if (state is FetchDeliveryGoodsLoading) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+
+          if (state is FetchDeliveryGoodsLoaded) {
+            if (state.batches.isEmpty) {
+              return const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 48),
                 child: Center(
                   child: Text(
@@ -61,19 +40,71 @@ class SendGoodsPage extends StatelessWidget {
                     textAlign: TextAlign.center,
                   ),
                 ),
-              ),
-            )
-          else
-            SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              sliver: SliverList.separated(
-                itemBuilder: (context, index) =>
-                    BatchCardQuantityItem(batch: batch),
-                separatorBuilder: (context, index) =>
-                    const SizedBox(height: 12),
-              ),
-            ),
-        ],
+              );
+            }
+
+            return CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  actions: <Widget>[
+                    NotificationIconButton(),
+                    const SizedBox(width: 16),
+                  ],
+                  expandedHeight: kToolbarHeight + kSpaceBarHeight,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Container(
+                      alignment: Alignment.bottomCenter,
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Cari batch pengiriman',
+                                prefixIcon: const Icon(Icons.search_outlined),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          DecoratedIconButton(
+                            onTap: () => context.push(sendGoodsFilterRoute),
+                            icon: const Icon(Icons.add_outlined),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  floating: true,
+                  snap: true,
+                  pinned: true,
+                  title: const Text('Kirim Barang'),
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  sliver: SliverList.separated(
+                    itemBuilder: (context, index) => BatchCardItem(
+                      onTap: () => showModalBottomSheet(
+                        builder: (context) => ShipmentReceiptNumbersBottomSheet(
+                          onSelected: (selectedReceiptNumbers) => context.push(
+                              '$itemDetailRoute/${selectedReceiptNumbers.first}'),
+                          batch: batch,
+                        ),
+                        backgroundColor: light,
+                        context: context,
+                      ),
+                      batch: state.batches[index],
+                    ),
+                    separatorBuilder: (context, index) =>
+                        const SizedBox(height: 12),
+                    itemCount: state.batches.length,
+                  ),
+                ),
+              ],
+            );
+          }
+
+          return const SizedBox();
+        },
       ),
     );
   }
