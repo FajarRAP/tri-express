8533532419464088b0bff12b67a9192f81305297
Fajar Riansyah <fajary781@gmail.com>
2025-09-11 16:21:59 +0700

    feat (inventory): enhance fetchReceiveGoods and fetchReceiveGoodsPaginate methods with search functionality and improved state management

--- lib/features/inventory/presentation/cubit/inventory_cubit.dart
+++ lib/features/inventory/presentation/cubit/inventory_cubit.dart
@@ -3,19 +3,24 @@ import 'package:meta/meta.dart';
 
 import '../../domain/entities/batch_entity.dart';
 import '../../domain/use_cases/fetch_delivery_goods_use_case.dart';
+import '../../domain/use_cases/fetch_receive_goods_use_case.dart';
 
 part 'inventory_state.dart';
 
 class InventoryCubit extends Cubit<InventoryState> {
   InventoryCubit({
     required FetchDeliveryGoodsUseCase fetchDeliveryGoodsUseCase,
+    required FetchReceiveGoodsUseCase fetchReceiveGoodsUseCase,
   })  : _fetchDeliveryGoodsUseCase = fetchDeliveryGoodsUseCase,
+        _fetchReceiveGoodsUseCase = fetchReceiveGoodsUseCase,
         super(InventoryInitial());
 
   final FetchDeliveryGoodsUseCase _fetchDeliveryGoodsUseCase;
+  final FetchReceiveGoodsUseCase _fetchReceiveGoodsUseCase;
 
   var _currentPage = 1;
   final _deliveryBatches = <BatchEntity>[];
+  final _receiveBatches = <BatchEntity>[];
 
   Future<void> fetchDeliveryGoods() async {
     emit(FetchDeliveryGoodsLoading());
@@ -63,11 +68,39 @@ class InventoryCubit extends Cubit<InventoryState> {
     emit(FetchPrepareGoodsLoaded(batches: []));
   }
 
-  Future<void> fetchReceiveGoods() async {
+  Future<void> fetchReceiveGoods({String? search}) async {
     emit(FetchReceiveGoodsLoading());
 
-    await Future.delayed(const Duration(seconds: 1));
+    final params = FetchReceiveGoodsUseCaseParams(
+      page: _currentPage = 1,
+      search: search,
+    );
+    final result = await _fetchReceiveGoodsUseCase(params);
 
-    emit(FetchReceiveGoodsLoaded(batches: []));
+    result.fold(
+      (failure) => emit(FetchReceiveGoodsError(message: failure.message)),
+      (batches) => emit(FetchReceiveGoodsLoaded(
+          batches: _receiveBatches
+            ..clear()
+            ..addAll(batches))),
+    );
+  }
+
+  Future<void> fetchReceiveGoodsPaginate({String? search}) async {
+    emit(ListPaginateLoading());
+
+    final params = FetchReceiveGoodsUseCaseParams(
+      page: ++_currentPage,
+      search: search,
+    );
+    final result = await _fetchReceiveGoodsUseCase(params);
+
+    result.fold(
+      (failure) => emit(ListPaginateError(message: failure.message)),
+      (batches) => batches.isEmpty
+          ? emit(ListPaginateLast(currentPage: _currentPage = 1))
+          : emit(FetchReceiveGoodsLoaded(
+              batches: _receiveBatches..addAll(batches))),
+    );
   }
 }
