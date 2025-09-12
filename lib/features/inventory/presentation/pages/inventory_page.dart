8c0a89df3b60b8ccc2eb749724b37e7764316d60
Fajar Riansyah <fajary781@gmail.com>
2025-09-12 16:59:37 +0700

    feat (inventory page): refactor to StatelessWidget and update UI with CustomScrollView

--- lib/features/inventory/presentation/pages/inventory_page.dart
+++ lib/features/inventory/presentation/pages/inventory_page.dart
@@ -1,183 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../core/fonts/fonts.dart';
+import '../../../../core/routes/router.dart';
 import '../../../../core/themes/colors.dart';
 import '../../../../core/utils/constants.dart';
-import '../../../../core/utils/helpers.dart';
-import '../../../../core/widgets/notification_icon_button.dart';
-import '../../../../main.dart';
-import '../../../../uhf_result_model.dart';
-import '../../domain/entity/batch_entity.dart';
-import '../widgets/batch_card_status_item.dart';
-import '../widgets/inventory_summary_card.dart';
+import '../../../../core/widgets/decorated_icon_button.dart';
+import '../../../../core/widgets/primary_gradient_card.dart';
+import '../widgets/batch_card_item.dart';
+import '../widgets/shipment_receipt_numbers_bottom_sheet.dart';
 
-class InventoryPage extends StatefulWidget {
+class InventoryPage extends StatelessWidget {
   const InventoryPage({super.key});
-
-  @override
-  State<InventoryPage> createState() => _InventoryPageState();
-}
-
-class _InventoryPageState extends State<InventoryPage> {
-  late final UHFMethodHandler _uhfMethodHandler;
-  final _tagInfos = <UHFResultModel>[];
-  var _isInventoryRunning = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _uhfMethodHandler = UHFMethodHandler(platform);
-    platform.setMethodCallHandler(
-      (call) async => await _uhfMethodHandler.methodHandler(
-        call,
-        onGetTag: (tagInfo) {
-          final index = _tagInfos.indexWhere((e) => e.epcId == tagInfo.epcId);
-
-          setState(() => index != -1
-              ? _tagInfos[index].updateInfo(tagInfo: tagInfo)
-              : _tagInfos.add(tagInfo));
-        },
-        onToggleInventory: (toggleCase, response) {
-          setState(() => _isInventoryRunning = response.statusCode == 1);
-
-          call.method == startInventoryMethod
-              ? TopSnackbar.successSnackbar(message: response.message)
-              : TopSnackbar.dangerSnackbar(message: response.message);
-        },
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Inventory Gudang'),
-        actions: <Widget>[
-          const NotificationIconButton(),
-          const SizedBox(width: 16),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 16),
-            // Header
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Cari resi atau invoice',
-                      prefixIcon: const Icon(Icons.search_outlined),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: whiteTertiary),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const <BoxShadow>[
-                      BoxShadow(
-                        blurRadius: 4,
-                        color: Color(0x4D3F3F3F),
-                        offset: Offset(0, 1),
-                        spreadRadius: 0,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: kToolbarHeight + kSpaceBarHeight + 128,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                alignment: Alignment.bottomCenter,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    SizedBox(
+                      width: double.infinity,
+                      child: PrimaryGradientCard(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Jumlah Koli di Gudang',
+                              style: paragraphMedium[bold].copyWith(
+                                color: light,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              '\$Number',
+                              style: heading5[bold].copyWith(
+                                color: light,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                    color: light,
-                  ),
-                  height: 40,
-                  width: 40,
-                  child: const Icon(Icons.qr_code_scanner_outlined),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: whiteTertiary),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const <BoxShadow>[
-                      BoxShadow(
-                        blurRadius: 4,
-                        color: Color(0x4D3F3F3F),
-                        offset: Offset(0, 1),
-                        spreadRadius: 0,
-                      ),
-                    ],
-                    color: light,
-                  ),
-                  height: 40,
-                  width: 40,
-                  child: const Icon(Icons.print_outlined),
-                ),
-              ],
-            ),
-            const SizedBox(height: 24),
-            // Cards
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: InventorySummaryCard(title: 'Koli', value: 0),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: InventorySummaryCard(title: 'Scan', value: 0),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: InventorySummaryCard(title: 'Hilang', value: 0),
-                ),
-              ],
-            ),
-            const SizedBox(height: 24),
-            // Datas
-            if (_tagInfos.isEmpty)
-              Expanded(
-                child: Center(
-                  child: Text(
-                    'Belum ada barang, terima barang sebelum cek inventory gudang',
-                    style: const TextStyle(
-                      color: grayTertiary,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              )
-            else
-              Expanded(
-                child: ListView.separated(
-                  itemBuilder: (context, index) => BatchCardStatusItem(
-                    batch: BatchEntity(
-                      id: '$index',
-                      batch: _tagInfos[index].epcId,
-                      destination: faker.address.city(),
-                      itemCount: faker.randomGenerator.integer(100, min: 1),
-                      origin: faker.address.city(),
-                      path: faker.randomGenerator.mapElementValue(shipmentPath),
-                      sendAt: faker.date.dateTime(),
-                      status:
-                          faker.randomGenerator.mapElementValue(shipmentStatus),
+                    const SizedBox(height: 24),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Cari resi atau invoice',
+                              prefixIcon: const Icon(Icons.search_outlined),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        DecoratedIconButton(
+                          onTap: () {},
+                          icon: const Icon(Icons.qr_code_scanner_outlined),
+                        ),
+                      ],
                     ),
-                  ),
-                  separatorBuilder: (context, index) =>
-                      const SizedBox(height: 12),
-                  itemCount: _tagInfos.length,
-                  padding: const EdgeInsets.only(bottom: 16),
+                  ],
                 ),
               ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _uhfMethodHandler.invokeHandleInventory,
-        backgroundColor: primary,
-        foregroundColor: light,
-        child: _isInventoryRunning
-            ? const Icon(Icons.stop)
-            : const Icon(Icons.play_arrow),
+            ),
+            floating: true,
+            pinned: true,
+            snap: true,
+            title: const Text('Inventory Gudang'),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverList.separated(
+              itemBuilder: (context, index) => BatchCardItem(
+                onTap: () => showModalBottomSheet(
+                  context: context,
+                  builder: (context) => ShipmentReceiptNumbersBottomSheet(
+                    onSelected: (selectedReceiptNumbers) => context.push(
+                        '$itemDetailRoute/${selectedReceiptNumbers.first}'),
+                    batch: batch,
+                  ),
+                ),
+                batch: batch,
+              ),
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
+              itemCount: null,
+            ),
+          ),
+        ],
       ),
     );
   }
