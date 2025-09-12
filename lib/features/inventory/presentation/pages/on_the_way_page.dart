c8e45417dfaae93b8de89e167847910a8e8ac3e4
Fajar Riansyah <fajary781@gmail.com>
2025-09-12 16:10:48 +0700

    feat (on the way page): refactor layout to use CustomScrollView and enhance UI elements

--- lib/features/inventory/presentation/pages/on_the_way_page.dart
+++ lib/features/inventory/presentation/pages/on_the_way_page.dart
@@ -1,61 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../core/fonts/fonts.dart';
+import '../../../../core/routes/router.dart';
 import '../../../../core/themes/colors.dart';
+import '../../../../core/utils/constants.dart';
 import '../../../../core/widgets/notification_icon_button.dart';
 import '../../../../core/widgets/primary_gradient_card.dart';
-import '../../domain/entity/batch_entity.dart';
-import '../widgets/batch_card_status_item.dart';
+import '../widgets/batch_card_item.dart';
+import '../widgets/shipment_receipt_numbers_bottom_sheet.dart';
 
 class OnTheWayPage extends StatelessWidget {
   const OnTheWayPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final batches = BatchEntity.generate(20);
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('On The Way'),
-        actions: <Widget>[
-          const NotificationIconButton(),
-          const SizedBox(width: 16),
-        ],
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        children: <Widget>[
-          PrimaryGradientCard(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Dalam Pengiriman ke \$Warehouse Location',
-                  style: const TextStyle(
-                    color: light,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                  ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            actions: <Widget>[
+              NotificationIconButton(),
+              const SizedBox(width: 16),
+            ],
+            expandedHeight: kToolbarHeight + kSpaceBarHeight + 128,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                alignment: Alignment.bottomCenter,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    SizedBox(
+                      width: double.infinity,
+                      child: PrimaryGradientCard(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Dalam Pengiriman ke \$Warehouse',
+                              style: paragraphMedium[bold].copyWith(
+                                color: light,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              '\$Number',
+                              style: heading5[bold].copyWith(color: light),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Cari resi atau invoice',
+                        prefixIcon: const Icon(Icons.search_outlined),
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  'Number',
-                  style: const TextStyle(
-                    color: light,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ],
+              ),
             ),
+            floating: true,
+            pinned: true,
+            snap: true,
+            title: const Text('On The Way'),
           ),
-          const SizedBox(height: 24),
-          ListView.separated(
-            itemBuilder: (context, index) =>
-                BatchCardStatusItem(batch: batches[index]),
-            separatorBuilder: (context, index) => const SizedBox(height: 24),
-            itemCount: batches.length,
-            padding: const EdgeInsets.only(bottom: 24),
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverList.separated(
+              itemBuilder: (context, index) => BatchCardItem(
+                onTap: () => showModalBottomSheet(
+                  context: context,
+                  builder: (context) => ShipmentReceiptNumbersBottomSheet(
+                    onSelected: (selectedReceiptNumbers) => context.push(
+                        '$itemDetailRoute/${selectedReceiptNumbers.first}'),
+                    batch: batch,
+                  ),
+                ),
+                batch: batch,
+              ),
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
+              itemCount: null,
+            ),
           )
         ],
       ),
