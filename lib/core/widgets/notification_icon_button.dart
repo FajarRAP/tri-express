d38e724d85f6e525d8e4185e4d9c0c52799f312a
Fajar Riansyah <fajary781@gmail.com>
2025-09-15 11:22:39 +0700

    feat (notification icon button): add navigation to notification page

--- lib/core/widgets/notification_icon_button.dart
+++ lib/core/widgets/notification_icon_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../routes/router.dart';
 import '../themes/colors.dart';
 
 class NotificationIconButton extends StatelessWidget {
@@ -8,20 +10,11 @@ class NotificationIconButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () => context.push(notificationRoute),
       child: Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(
           border: Border.all(color: graySecondary),
-          // boxShadow: <BoxShadow>[
-          //   BoxShadow(
-          //     blurRadius: 4,
-          //     color: Color(0x4D3F3F3F),
-          //     offset: const Offset(0, 1),
-          //     spreadRadius: 0,
-          //   )
-          // ],
-          // color: Color(0xFFEEEEEE),
           shape: BoxShape.circle,
         ),
         padding: const EdgeInsets.all(10),
@@ -31,5 +24,29 @@ class NotificationIconButton extends StatelessWidget {
         ),
       ),
     );
+
+    // return GestureDetector(
+    //   onTap: () => context.push(notificationRoute),
+    //   child: Container(
+    //     alignment: Alignment.center,
+    //     padding: const EdgeInsets.all(10),
+    //     decoration: BoxDecoration(
+    //       shape: BoxShape.circle,
+    //       color: Colors.white, // Warna dasar untuk container
+    //       gradient: RadialGradient(
+    //         colors: [
+    //           Colors.white, // Warna di tengah (sesuaikan dengan warna dasar)
+    //           Colors.grey.shade400, // Warna bayangan di tepi
+    //         ],
+    //         center: Alignment.center,
+    //         stops: const [0.8, 1.0], // Mengatur ketebalan bayangan di tepi
+    //       ),
+    //     ),
+    //     child: Icon(
+    //       Icons.notifications_outlined,
+    //       color: Colors.black, // Variabel 'black' dari kode Anda
+    //     ),
+    //   ),
+    // );
   }
 }
