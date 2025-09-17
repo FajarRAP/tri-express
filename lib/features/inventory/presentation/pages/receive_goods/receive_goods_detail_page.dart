c3432066fb0f5be7d63c6be271aa91410c90f1ca
Fajar Riansyah <fajary781@gmail.com>
2025-09-17 12:10:32 +0700

    feat: add receive goods detail page

---
+++ lib/features/inventory/presentation/pages/receive_goods/receive_goods_detail_page.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/fonts/fonts.dart';
+import '../../../../../core/themes/colors.dart';
+import '../../../../../core/utils/helpers.dart';
+import '../../../../../core/widgets/base_card.dart';
+import '../../../domain/entities/good_entity.dart';
+import '../../widgets/info_tile.dart';
+
+class ReceiveGoodsDetailPage extends StatelessWidget {
+  const ReceiveGoodsDetailPage({
+    super.key,
+    required this.batchName,
+    required this.good,
+    required this.receiveAt,
+  });
+
+  final String batchName;
+  final GoodEntity good;
+  final DateTime receiveAt;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail Barang'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: BaseCard(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Informasi Barang',
+                  style: paragraphSmall[heavy].copyWith(
+                    color: black,
+                  ),
+                ),
+                const SizedBox(height: 12),
+                InfoTile(
+                  title: 'Nama Barang',
+                  value: good.name,
+                ),
+                const SizedBox(height: 8),
+                InfoTile(
+                  title: 'Tanggal Terima',
+                  value: receiveAt.toDDMMMMYYYY,
+                ),
+                const SizedBox(height: 8),
+                InfoTile(
+                  title: 'Customer',
+                  value: good.customer.name,
+                ),
+                const SizedBox(height: 8),
+                InfoTile(
+                  title: 'Batch',
+                  value: batchName,
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: InfoTile(
+                        title: 'Total Koli',
+                        value: '${good.totalItem}',
+                      ),
+                    ),
+                    Expanded(
+                      child: InfoTile(
+                        title: 'Jalur',
+                        value: good.transportMode,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: InfoTile(
+                        title: 'No Invoice',
+                        value: good.invoiceNumber,
+                      ),
+                    ),
+                    Expanded(
+                      child: InfoTile(
+                        title: 'No Resi',
+                        value: good.receiptNumber,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: InfoTile(
+                        title: 'Gudang Awal',
+                        value: good.origin.name,
+                      ),
+                    ),
+                    Expanded(
+                      child: InfoTile(
+                        title: 'Gudang Akhir',
+                        value: good.destination.name,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'Informasi Koli',
+                  style: TextStyle(fontSize: 12, color: gray),
+                ),
+                ListView.separated(
+                  itemBuilder: (context, index) {
+                    final firstIndex = index * 2;
+                    final secondIndex = firstIndex + 1;
+
+                    return Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            good.uniqueCodes[firstIndex],
+                            style: label[medium].copyWith(color: black),
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            good.uniqueCodes[secondIndex],
+                            style: label[medium].copyWith(color: black),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(height: 8),
+                  itemCount: (good.uniqueCodes.length / 2).ceil(),
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
