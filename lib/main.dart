1fb80836b257f5b84ba706b00b3ae90f308313a6
Fajar Riansyah <fajary781@gmail.com>
2025-09-25 15:59:29 +0700

    refactor (main): remove initialLocation check in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
-import 'package:faker/faker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'core/routes/router.dart';
 import 'core/themes/theme.dart';
-import 'core/utils/constants.dart';
 import 'core/utils/top_snackbar.dart';
-import 'features/auth/data/data_sources/auth_local_data_sources.dart';
 import 'features/auth/presentation/cubit/auth_cubit.dart';
 import 'features/core/presentation/cubit/core_cubit.dart';
 import 'features/inventory/presentation/cubit/inventory_cubit.dart';
 import 'firebase_options.dart';
 import 'service_locator.dart';
-
-late Faker faker;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,15 +18,6 @@ void main() async {
   );
 
   setupServiceLocator();
-
-  faker = Faker();
-  final storage = getIt.get<FlutterSecureStorage>();
-  final authLocal = getIt.get<AuthLocalDataSources>();
-  initialLocation = await storage.read(key: onboardingKey) == null
-      ? onboardingRoute
-      : await authLocal.getAccessToken() != null
-          ? menuRoute
-          : loginRoute;
 
   runApp(const MyApp());
 }
@@ -44,10 +29,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<CoreCubit>(create: (context) => getIt.get<CoreCubit>()),
-        BlocProvider<AuthCubit>(create: (context) => getIt.get<AuthCubit>()),
+        BlocProvider<CoreCubit>(create: (context) => getIt<CoreCubit>()),
+        BlocProvider<AuthCubit>(create: (context) => getIt<AuthCubit>()),
         BlocProvider<InventoryCubit>(
-            create: (context) => getIt.get<InventoryCubit>()),
+            create: (context) => getIt<InventoryCubit>()),
       ],
       child: MaterialApp.router(
         builder: (context, child) => Overlay(
