4526d58a79d9cc8c5e61749c4da752f49abd889a
Fajar Riansyah <fajary781@gmail.com>
2025-09-26 16:10:30 +0700

    feat (pick up goods scan page): implement fetch preview pick up goods use case

--- lib/features/inventory/presentation/pages/pick_up_goods/pick_up_goods_scan_page.dart
+++ lib/features/inventory/presentation/pages/pick_up_goods/pick_up_goods_scan_page.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../../../../../core/fonts/fonts.dart';
 import '../../../../../core/routes/router.dart';
 import '../../../../../core/themes/colors.dart';
 import '../../../../../core/utils/constants.dart';
-import '../../../../../core/utils/top_snackbar.dart';
 import '../../../../../core/utils/uhf_utils.dart';
 import '../../../../../core/widgets/action_confirmation_bottom_sheet.dart';
 import '../../../../../core/widgets/decorated_icon_button.dart';
 import '../../../../../core/widgets/primary_gradient_card.dart';
 import '../../../../../core/widgets/triple_floating_action_buttons.dart';
-import '../../../../core/domain/entities/uhf_result_entity.dart';
+import '../../../../auth/presentation/cubit/auth_cubit.dart';
+import '../../cubit/inventory_cubit.dart';
 import '../../widgets/good_card_checkbox.dart';
-import '../../widgets/shipment_receipt_numbers_bottom_sheet.dart';
+import '../../widgets/scanned_item_card.dart';
+import '../../widgets/unique_codes_bottom_sheet.dart';
 
 class PickUpGoodsScanPage extends StatefulWidget {
   const PickUpGoodsScanPage({super.key});
@@ -22,37 +25,26 @@ class PickUpGoodsScanPage extends StatefulWidget {
   State<PickUpGoodsScanPage> createState() => _PickUpGoodsScanPageState();
 }
 
-class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
-  late final UHFMethodHandler _uhfMethodHandler;
-  final _tagInfos = <UHFResultEntity>[];
-  final _selectedItemIds = <String>{};
-  var _isInventoryRunning = false;
-  var _isSelectAll = false;
+class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage>
+    with UHFMethodHandlerMixin {
+  late final AuthCubit _authCubit;
+  late final InventoryCubit _inventoryCubit;
+  final _selectedCodes = <String, Set<String>>{};
 
   @override
   void initState() {
     super.initState();
-    _uhfMethodHandler = const UHFMethodHandler(platform);
-    platform.setMethodCallHandler(
-      (call) async => await _uhfMethodHandler.methodHandler(
-        call,
-        onGetTag: (tagInfo) {
-          final index = _tagInfos.indexWhere((e) => e.epcId == tagInfo.epcId);
-
-          setState(() => index != -1
-              ? _tagInfos[index].updateInfo(tagInfo: tagInfo)
-              : _tagInfos.add(tagInfo));
-        },
-        onToggleInventory: (toggleCase, response) {
-          setState(() => _isInventoryRunning = response.statusCode == 1);
-
-          call.method == startInventoryMethod
-              ? TopSnackbar.successSnackbar(message: response.message)
-              : TopSnackbar.dangerSnackbar(message: response.message);
-        },
-      ),
-    );
+    _authCubit = context.read<AuthCubit>();
+    _inventoryCubit = context.read<InventoryCubit>();
+    initUHFMethodHandler(platform);
   }
+
+  @override
+  InventoryCubit get inventoryCubit => _inventoryCubit;
+
+  @override
+  void Function() get onInventoryStop =>
+      () => _inventoryCubit.fetchPreviewPickUpShipments(uhfResults: uhfResults);
 
   @override
   Widget build(BuildContext context) {
@@ -75,21 +67,21 @@ class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Text(
-                              'Barang di Gudang \$Warehouse',
+                              'Barang di ${_authCubit.user.warehouse?.name}',
                               style: paragraphMedium[bold].copyWith(
                                 color: light,
                               ),
                             ),
                             const SizedBox(height: 8),
                             Text(
-                              'Total Koli',
+                              'Total Koli Terscan',
                               style: paragraphSmall[medium].copyWith(
                                 color: light,
                               ),
                             ),
                             const SizedBox(height: 8),
                             Text(
-                              '\$Number',
+                              '${uhfResults.length}',
                               style: paragraphSmall[medium].copyWith(
                                 color: light,
                               ),
@@ -111,7 +103,13 @@ class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
                         ),
                         const SizedBox(width: 10),
                         DecoratedIconButton(
-                          onTap: () => context.push(scanBarcodeInnerRoute),
+                          onTap: () async {
+                            final result = await context
+                                .push<Barcode>(scanBarcodeInnerRoute);
+                            if (result == null) return;
+
+                            onQRScan('${result.displayValue}');
+                          },
                           icon: const Icon(Icons.qr_code_scanner_outlined),
                         ),
                       ],
@@ -144,14 +142,13 @@ class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
                 onChanged: (value) {
                   if (value == null) return;
 
-                  _isSelectAll = value;
-                  _selectedItemIds.clear();
-
                   setState(() {
-                    if (_isSelectAll) {
-                      for (var good in goods) {
-                        _selectedItemIds.add(good.id);
+                    if (value) {
+                      for (final good in _inventoryCubit.previewGoods) {
+                        _selectedCodes[good.id] = good.uniqueCodes.toSet();
                       }
+                    } else {
+                      _selectedCodes.clear();
                     }
                   });
                 },
@@ -159,13 +156,18 @@ class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
                   'Semua',
                   style: label[medium].copyWith(color: primary),
                 ),
-                value: _isSelectAll,
+                value: _inventoryCubit.previewGoods.every((good) =>
+                        (_selectedCodes[good.id]?.length ?? 0) ==
+                        good.uniqueCodes.length) &&
+                    _inventoryCubit.previewGoods.isNotEmpty,
                 side: const BorderSide(color: primary),
                 controlAffinity: ListTileControlAffinity.leading,
               ),
             ),
             Expanded(
               child: TripleFloatingActionButtons(
+                onReset: onReset,
+                onScan: onScan,
                 onSave: () => showModalBottomSheet(
                   context: context,
                   builder: (context) => ActionConfirmationBottomSheet(
@@ -175,7 +177,7 @@ class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
                     message: 'Apakah anda yakin akan mengirim barang ini?',
                   ),
                 ),
-                isScanning: _isInventoryRunning,
+                isScanning: isInventoryRunning,
               ),
             ),
           ],
@@ -186,7 +188,7 @@ class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
   }
 
   Widget _buildList() {
-    if (_tagInfos.isNotEmpty) {
+    if (uhfResults.isEmpty) {
       return SliverFillRemaining(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40),
@@ -201,31 +203,74 @@ class _PickUpGoodsScanPageState extends State<PickUpGoodsScanPage> {
       );
     }
 
-    return SliverPadding(
-      padding: const EdgeInsets.only(bottom: 80),
-      sliver: SliverList.separated(
-        itemBuilder: (context, index) => GoodCardCheckbox(
-          onChanged: (value) {
-            if (value == null) return;
-
-            setState(() => value
-                ? _selectedItemIds.add(goods[index].id)
-                : _selectedItemIds.remove(goods[index].id));
-          },
-          onTap: () => showModalBottomSheet(
-            context: context,
-            builder: (context) => ShipmentReceiptNumbersBottomSheet(
-              onSelected: (selectedReceiptNumbers) =>
-                  context.push(pickUpGoodsConfirmationRoute),
-              batch: batch,
+    return BlocBuilder<InventoryCubit, InventoryState>(
+      buildWhen: (previous, current) =>
+          current is FetchPreviewGoodsShipments || current is UHFAction,
+      builder: (context, state) {
+        if (state is FetchPreviewGoodsShipmentsLoading) {
+          return const SliverFillRemaining(
+            hasScrollBody: false,
+            child: Center(
+              child: CircularProgressIndicator.adaptive(),
             ),
-          ),
-          good: goods[index],
-          isActive: _selectedItemIds.contains(goods[index].id),
-        ),
-        separatorBuilder: (context, index) => const SizedBox(height: 12),
-        itemCount: null,
-      ),
+          );
+        }
+
+        if (state is FetchPreviewGoodsShipmentsLoaded) {
+          return SliverPadding(
+            padding: const EdgeInsets.only(bottom: 80),
+            sliver: SliverList.separated(
+              itemBuilder: (context, index) => GoodCardCheckbox(
+                onChanged: (value) {
+                  if (value == null) return;
+
+                  setState(() => value
+                      ? _selectedCodes[state.goods[index].id] =
+                          state.goods[index].uniqueCodes.toSet()
+                      : _selectedCodes.remove(state.goods[index].id));
+                },
+                onTap: () => showModalBottomSheet(
+                  context: context,
+                  builder: (context) => UniqueCodesBottomSheet(
+                    onSelected: (selectedCodes) {
+                      context.pop();
+                      setState(() => selectedCodes.isEmpty
+                          ? _selectedCodes.remove(state.goods[index].id)
+                          : _selectedCodes[state.goods[index].id] =
+                              selectedCodes.toSet());
+                    },
+                    goodName: state.goods[index].name,
+                    selectedCodes:
+                        _selectedCodes[state.goods[index].id]?.toList() ?? [],
+                    uniqueCodes: state.goods[index].uniqueCodes,
+                  ),
+                ),
+                good: state.goods[index],
+                isActive: _selectedCodes.containsKey(state.goods[index].id),
+                selectedCodesCount:
+                    _selectedCodes[state.goods[index].id]?.length ?? 0,
+              ),
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
+              itemCount: state.goods.length,
+            ),
+          );
+        }
+
+        if (state is OnUHFScan || state is QRCodeScan) {
+          return SliverPadding(
+            padding: const EdgeInsets.only(bottom: 80),
+            sliver: SliverList.builder(
+              itemBuilder: (context, index) => ScannedItemCard(
+                number: index + 1,
+                item: uhfResults[index],
+              ),
+              itemCount: uhfResults.length,
+            ),
+          );
+        }
+
+        return const SliverToBoxAdapter();
+      },
     );
   }
 }
