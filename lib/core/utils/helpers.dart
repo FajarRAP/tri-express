6705e907f6c45ccec8e94658ae8373f9bea9d827
Fajar Riansyah <fajary781@gmail.com>
2025-10-09 15:53:03 +0700

    feat (helpers): add pagination helper for scroll notifications

--- lib/core/utils/helpers.dart
+++ lib/core/utils/helpers.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 import '../exceptions/server_exception.dart';
@@ -16,8 +17,19 @@ ServerException handleDioException(DioException de) {
   }
 }
 
+bool paginateWhenScrollEnd(ScrollNotification scrollState,
+    {required void Function() paginate}) {
+  if (scrollState is ScrollEndNotification) {
+    paginate();
+  }
+
+  return false;
+}
+
 extension DateTimeExtension on DateTime {
   String get toDDMMMMYYYY => DateFormat('dd MMMM yyyy').format(this);
   String get toDDMMMM => DateFormat('dd MMMM').format(this);
   String get toHHmm => DateFormat('HH:mm').format(this);
+  String get toYYYYMMDD => DateFormat('yyyy-MM-dd').format(this);
+  String get toDDMMMYYY => DateFormat('dd MMM yyyy').format(this);
 }
