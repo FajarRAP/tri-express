cbd41c5ec9bdabd5e9471746e6af193e0f3bd4c4
Fajar Riansyah <fajary781@gmail.com>
2025-10-10 16:00:13 +0700

    feat (unique code input bottom sheet): refactor to use StatefulWidget and manage input state

--- lib/features/inventory/presentation/widgets/unique_code_input_bottom_sheet.dart
+++ lib/features/inventory/presentation/widgets/unique_code_input_bottom_sheet.dart
@@ -4,13 +4,34 @@ import 'package:go_router/go_router.dart';
 import '../../../../core/widgets/buttons/outline_primary_button.dart';
 import '../../../../core/widgets/buttons/primary_button.dart';
 
-class UniqueCodeInputBottomSheet extends StatelessWidget {
+class UniqueCodeInputBottomSheet extends StatefulWidget {
   const UniqueCodeInputBottomSheet({
     super.key,
     this.onPressed,
   });
 
-  final void Function()? onPressed;
+  final void Function(String value)? onPressed;
+
+  @override
+  State<UniqueCodeInputBottomSheet> createState() =>
+      _UniqueCodeInputBottomSheetState();
+}
+
+class _UniqueCodeInputBottomSheetState
+    extends State<UniqueCodeInputBottomSheet> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +42,11 @@ class UniqueCodeInputBottomSheet extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           TextFormField(
+            autofocus: true,
+            controller: _controller,
             decoration: const InputDecoration(
               labelText: 'Kode Koli',
             ),
-            autofocus: true,
             textInputAction: TextInputAction.done,
           ),
           const SizedBox(height: 24),
@@ -39,7 +61,9 @@ class UniqueCodeInputBottomSheet extends StatelessWidget {
               const SizedBox(width: 16),
               Expanded(
                 child: PrimaryButton(
-                  onPressed: onPressed,
+                  onPressed: widget.onPressed == null
+                      ? null
+                      : () => widget.onPressed!(_controller.text),
                   child: const Text('OK'),
                 ),
               ),
