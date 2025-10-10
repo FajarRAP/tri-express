c16f76e8edbc3b49c39832956c2958a8dcc6b09d
Fajar Riansyah <fajary781@gmail.com>
2025-10-10 16:49:57 +0700

    refactor (scanned item card): update leading widget to use SvgPicture and adjust title text

--- lib/features/inventory/presentation/widgets/scanned_item_card.dart
+++ lib/features/inventory/presentation/widgets/scanned_item_card.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../../../core/fonts/fonts.dart';
 import '../../../../core/themes/colors.dart';
+import '../../../../core/utils/constants.dart';
 import '../../../core/domain/entities/uhf_result_entity.dart';
 
 class ScannedItemCard extends StatelessWidget {
   const ScannedItemCard({
     super.key,
-    required this.number,
     required this.item,
   });
 
-  final int number;
   final UHFResultEntity item;
 
   @override
@@ -23,15 +23,23 @@ class ScannedItemCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
       ),
       child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: primary,
-          child: Text(
-            '$number',
-            style: label[semibold].copyWith(color: light),
+        leading: Container(
+          decoration: const BoxDecoration(
+            color: primary50,
+            shape: BoxShape.circle,
+          ),
+          padding: const EdgeInsets.all(10),
+          child: SvgPicture.asset(
+            boxSvgPath,
+            colorFilter: const ColorFilter.mode(
+              primary,
+              BlendMode.srcIn,
+            ),
+            width: 20,
           ),
         ),
         title: const Text(
-          'Kode EPC Terbaca',
+          'Nomor Koli',
           style: TextStyle(
             color: grayTertiary,
             fontSize: 12,
