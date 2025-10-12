0a7f9f5cc361ecfa8535c5751eef6dfabe9f4933
Fajar Riansyah <fajary781@gmail.com>
2025-10-12 19:13:06 +0700

    refactor (receive goods scan page): change cubit to receive cubit

--- lib/features/inventory/presentation/pages/receive_goods/receive_goods_scan_page.dart
+++ lib/features/inventory/presentation/pages/receive_goods/receive_goods_scan_page.dart
@@ -6,6 +6,7 @@ import '../../../../../core/fonts/fonts.dart';
 import '../../../../../core/routes/router.dart';
 import '../../../../../core/themes/colors.dart';
 import '../../../../../core/utils/constants.dart';
+import '../../../../../core/utils/states.dart';
 import '../../../../../core/utils/top_snackbar.dart';
 import '../../../../../core/utils/uhf_utils.dart';
 import '../../../../../core/widgets/action_confirmation_bottom_sheet.dart';
@@ -15,7 +16,8 @@ import '../../../../../core/widgets/floating_action_button_bar.dart';
 import '../../../../auth/presentation/cubit/auth_cubit.dart';
 import '../../../../core/domain/entities/dropdown_entity.dart';
 import '../../../domain/entities/batch_entity.dart';
-import '../../cubit/inventory_cubit.dart';
+import '../../cubit/receive_cubit.dart';
+import '../../cubit/scanner_cubit.dart';
 import '../../widgets/batch_card_item.dart';
 import '../../widgets/scanned_item_card.dart';
 import '../../widgets/unique_code_action_bottom_sheet.dart';
@@ -35,25 +37,29 @@ class ReceiveGoodsScanPage extends StatefulWidget {
 }
 
 class _ReceiveGoodsScanPageState extends State<ReceiveGoodsScanPage>
-    with UHFMethodHandlerMixin {
+    with UHFMethodHandlerMixinV2 {
   late final AuthCubit _authCubit;
-  late final InventoryCubit _inventoryCubit;
+  late final ReceiveCubit _receiveCubit;
+  late final ScannerCubit _scannerCubit;
 
   @override
   void initState() {
     super.initState();
     _authCubit = context.read<AuthCubit>();
-    _inventoryCubit = context.read<InventoryCubit>()..resetState();
+    _receiveCubit = context.read<ReceiveCubit>()
+      ..clearBatches()
+      ..resetState();
+    _scannerCubit = context.read<ScannerCubit>();
     initUHFMethodHandler(platform);
   }
 
   @override
-  InventoryCubit get inventoryCubit => _inventoryCubit;
+  ScannerCubit get scannerCubit => _scannerCubit;
 
   @override
   void Function() get onInventoryStop =>
-      () => _inventoryCubit.fetchPreviewReceiveShipments(
-          origin: widget.origin, uhfresults: uhfResults);
+      () => _receiveCubit.fetchPreviewReceiveShipments(
+          origin: widget.origin, uhfresults: _scannerCubit.state.uhfResults);
 
   @override
   Widget build(BuildContext context) {
@@ -92,7 +98,7 @@ class _ReceiveGoodsScanPageState extends State<ReceiveGoodsScanPage>
                       children: <Widget>[
                         Expanded(
                           child: TextField(
-                            onChanged: _inventoryCubit.searchBatches,
+                            onChanged: _receiveCubit.searchBatches,
                             decoration: const InputDecoration(
                               hintText: 'Cari resi atau invoice',
                               prefixIcon: const Icon(Icons.search_outlined),
@@ -103,8 +109,18 @@ class _ReceiveGoodsScanPageState extends State<ReceiveGoodsScanPage>
                         DecoratedIconButton(
                           onTap: () => showModalBottomSheet(
                             context: context,
-                            builder: (context) =>
-                                UniqueCodeActionBottomSheet(onResult: onQRScan),
+                            builder: (context) => UniqueCodeActionBottomSheet(
+                              onResult: (value) {
+                                if (_scannerCubit.state.uhfResults
+                                    .any((e) => e.epcId == value)) {
+                                  return TopSnackbar.dangerSnackbar(
+                                      message: 'Kode sudah discan');
+                                }
+                                onQRScan(value);
+                                TopSnackbar.successSnackbar(
+                                    message: 'Berhasil ditambahkan');
+                              },
+                            ),
                           ),
                           icon: const Icon(Icons.qr_code_scanner_outlined),
                         ),
@@ -131,41 +147,63 @@ class _ReceiveGoodsScanPageState extends State<ReceiveGoodsScanPage>
           color: light,
         ),
         padding: const EdgeInsets.all(8),
-        child: BlocListener<InventoryCubit, InventoryState>(
-          listener: (context, state) {
-            if (state is CreateShipmentsLoaded) {
-              TopSnackbar.successSnackbar(message: state.message);
-              context.goNamed(receiveGoodsRoute);
-            }
+        child: BlocBuilder<ScannerCubit, ScannerState>(
+          builder: (context, scannerState) {
+            return SizedBox(
+              width: double.infinity,
+              child: FloatingActionButtonBar(
+                onReset: () {
+                  _receiveCubit.clearBatches();
+                  onReset();
+                },
+                onScan: onScan,
+                onSave: () => showModalBottomSheet(
+                  context: context,
+                  builder: (context) => BlocConsumer<ReceiveCubit,
+                      ReusableState<List<BatchEntity>>>(
+                    listener: (context, state) {
+                      if (state is ActionSuccess<List<BatchEntity>>) {
+                        TopSnackbar.successSnackbar(message: state.message);
+                        context.goNamed(receiveGoodsRoute);
+                      }
 
-            if (state is CreateShipmentsError) {
-              TopSnackbar.dangerSnackbar(message: state.message);
-            }
-          },
-          child: FloatingActionButtonBar(
-            onReset: onReset,
-            onScan: onScan,
-            onSave: () => showModalBottomSheet(
-              context: context,
-              builder: (context) => BlocBuilder<InventoryCubit, InventoryState>(
-                builder: (context, state) {
-                  final onPressed = switch (state) {
-                    CreateShipmentsLoading() => null,
-                    _ => () => _inventoryCubit.createReceiveShipments(
-                        uhfresults: uhfResults, receivedAt: widget.receivedAt),
-                  };
+                      if (state is ActionFailure<List<BatchEntity>>) {
+                        TopSnackbar.dangerSnackbar(
+                            message: state.failure.message);
+                      }
+                    },
+                    builder: (context, state) {
+                      final onPressed = switch (state) {
+                        ActionInProgress() => null,
+                        _ => () => _receiveCubit.createReceiveShipments(
+                            uhfresults: scannerState.uhfResults,
+                            receivedAt: widget.receivedAt),
+                      };
 
-                  return ActionConfirmationBottomSheet(
-                    onPressed: onPressed,
-                    message: 'Apakah anda yakin akan menyimpan barang ini?',
+                      return ActionConfirmationBottomSheet(
+                        onPressed: onPressed,
+                        message: 'Apakah anda yakin akan menyimpan barang ini?',
+                      );
+                    },
+                  ),
+                ),
+                onSync: () {
+                  if (scannerState.uhfResults.isEmpty) {
+                    return TopSnackbar.dangerSnackbar(
+                      message: 'Belum ada barang yang discan',
+                    );
+                  }
+
+                  _scannerCubit.updateInventoryStatus(false);
+                  _receiveCubit.fetchPreviewReceiveShipments(
+                    origin: widget.origin,
+                    uhfresults: scannerState.uhfResults,
                   );
                 },
+                isScanning: scannerState.isFromUHFReader,
               ),
-            ),
-            onSync: () => _inventoryCubit.fetchPreviewReceiveShipments(
-                origin: widget.origin, uhfresults: uhfResults),
-            isScanning: isInventoryRunning,
-          ),
+            );
+          },
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -174,77 +212,16 @@ class _ReceiveGoodsScanPageState extends State<ReceiveGoodsScanPage>
   }
 
   Widget _buildList() {
-    if (uhfResults.isEmpty) {
-      return SliverFillRemaining(
-        hasScrollBody: false,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Center(
-            child: Text(
-              'Belum ada item di gudang, klik pada icon scan untuk menerima item dengan RFID',
-              style: label[medium].copyWith(color: primaryGradientEnd),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-      );
-    }
-
-    return BlocBuilder<InventoryCubit, InventoryState>(
-      buildWhen: (previous, current) =>
-          current is FetchPreviewBatchesShipments || current is UHFAction,
-      builder: (context, state) {
-        if (state is FetchPreviewBatchesShipmentsLoading) {
-          return const SliverFillRemaining(
-            hasScrollBody: false,
-            child: Center(
-              child: CircularProgressIndicator.adaptive(),
-            ),
-          );
-        }
-
-        if (state is FetchPreviewBatchesShipmentsLoaded) {
-          if (state.batches.isEmpty) {
-            return SliverFillRemaining(
-              hasScrollBody: false,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: Center(
-                  child: Text(
-                    'Pengiriman tidak ditemukan, silahkan cek nama batch atau kode pengiriman',
-                    style: label[medium].copyWith(color: primaryGradientEnd),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            );
-          }
-
-          return SliverPadding(
-            padding: const EdgeInsets.only(bottom: 80),
-            sliver: SliverList.separated(
-              itemBuilder: (context, index) => BatchCardItem(
-                onTap: () => context.pushNamed(
-                  receiptNumbersRoute,
-                  extra: state.batches[index],
-                ),
-                batch: state.batches[index],
-                quantity: _renderQuantity(state.batches[index]),
-              ),
-              separatorBuilder: (context, index) => const SizedBox(height: 12),
-              itemCount: state.batches.length,
-            ),
-          );
-        }
-
-        if (state is FetchPreviewBatchesShipmentsError) {
+    return BlocBuilder<ScannerCubit, ScannerState>(
+      builder: (context, scannerState) {
+        if (scannerState.uhfResults.isEmpty) {
           return SliverFillRemaining(
             hasScrollBody: false,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: Center(
                 child: Text(
-                  state.message,
+                  'Belum ada item di gudang, klik pada icon scan untuk menerima item dengan RFID',
                   style: label[medium].copyWith(color: primaryGradientEnd),
                   textAlign: TextAlign.center,
                 ),
@@ -253,49 +230,118 @@ class _ReceiveGoodsScanPageState extends State<ReceiveGoodsScanPage>
           );
         }
 
-        if (state is OnUHFScan || state is QRCodeScan) {
+        if (scannerState.isFromQRScanner || scannerState.isFromUHFReader) {
           return SliverPadding(
             padding: const EdgeInsets.only(bottom: 80),
             sliver: SliverList.builder(
               itemBuilder: (context, index) =>
-                  ScannedItemCard(item: uhfResults[index]),
-              itemCount: uhfResults.length,
+                  ScannedItemCard(item: scannerState.uhfResults[index]),
+              itemCount: scannerState.uhfResults.length,
             ),
           );
         }
 
-        return const SliverToBoxAdapter();
+        return BlocBuilder<ReceiveCubit, ReusableState<List<BatchEntity>>>(
+          buildWhen: (previous, current) => current is FetchPreviewShipments,
+          builder: (context, state) {
+            if (state is FetchPreviewShipmentsLoading) {
+              return const SliverFillRemaining(
+                hasScrollBody: false,
+                child: Center(
+                  child: CircularProgressIndicator.adaptive(),
+                ),
+              );
+            }
+
+            if (state is FetchPreviewShipmentsLoaded) {
+              if (state.filteredBatches.isEmpty) {
+                return SliverFillRemaining(
+                  child: Center(
+                    child: Text(
+                      'Tidak ada data yang sesuai.',
+                      style: label[medium].copyWith(color: primaryGradientEnd),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                );
+              }
+
+              return SliverPadding(
+                padding: const EdgeInsets.only(bottom: 80),
+                sliver: SliverList.separated(
+                  itemBuilder: (context, index) => BatchCardItem(
+                    onTap: () => context.pushNamed(
+                      receiptNumbersRoute,
+                      extra: {
+                        'batch': state.data[index],
+                        'routeDetailName': receiveGoodsDetailRoute,
+                      },
+                    ),
+                    batch: state.filteredBatches[index],
+                    quantity: _renderQuantity(state.filteredBatches[index]),
+                  ),
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(height: 12),
+                  itemCount: state.filteredBatches.length,
+                ),
+              );
+            }
+
+            if (state is FetchPreviewShipmentsError) {
+              return SliverFillRemaining(
+                child: Center(
+                  child: Text(
+                    state.failure.message,
+                    style: label[medium].copyWith(color: primaryGradientEnd),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            }
+
+            return const SliverToBoxAdapter();
+          },
+        );
       },
     );
   }
 
   Widget _buildTotalUnits() {
-    return BlocBuilder<InventoryCubit, InventoryState>(
-      bloc: _inventoryCubit,
-      buildWhen: (previous, current) =>
-          current is FetchPreviewBatchesShipments || current is UHFAction,
-      builder: (context, state) {
-        final (String title, String value) = switch (state) {
-          FetchPreviewBatchesShipmentsLoading() => ('Memuat', '...'),
-          FetchPreviewBatchesShipmentsLoaded s => (
-              'Total Pengiriman',
-              '${s.batches.length}'
-            ),
-          OnUHFScan() || QRCodeScan() => (
-              'Total Koli Terscan',
-              '${uhfResults.length}'
-            ),
-          _ => ('Mulai scan untuk melihat pengiriman', '...'),
-        };
-        final style = paragraphSmall[medium].copyWith(color: light);
+    final style = paragraphSmall[medium].copyWith(color: light);
 
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title, style: style),
-            const SizedBox(height: 8),
-            Text(value, style: style),
-          ],
+    return BlocBuilder<ScannerCubit, ScannerState>(
+      builder: (context, scannerState) {
+        if (scannerState.isFromQRScanner || scannerState.isFromUHFReader) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Total Koli Terscan', style: style),
+              const SizedBox(height: 8),
+              Text('${scannerState.uhfResults.length}', style: style),
+            ],
+          );
+        }
+
+        return BlocBuilder<ReceiveCubit, ReusableState<List<BatchEntity>>>(
+          builder: (context, state) {
+            final (String title, String value) = switch (state) {
+              FetchPreviewShipmentsLoading() => ('Memuat', '-'),
+              FetchPreviewShipmentsLoaded s => (
+                  'Total Pengiriman',
+                  '${s.allBatches.length}'
+                ),
+              _ => ('Mulai scan untuk melihat pengiriman', '-'),
+            };
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: style),
+                const SizedBox(height: 8),
+                Text(value, style: style),
+              ],
+            );
+          },
         );
       },
     );
@@ -307,7 +353,7 @@ class _ReceiveGoodsScanPageState extends State<ReceiveGoodsScanPage>
         text: TextSpan(
           children: <InlineSpan>[
             TextSpan(
-              text: '${batch.goods.length}',
+              text: '${batch.totalAllUnits}',
               style: label[regular].copyWith(color: black),
             ),
             TextSpan(
