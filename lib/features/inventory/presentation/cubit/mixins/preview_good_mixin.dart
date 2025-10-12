7758ca3790e63954353a578a9579faa70d995687
Fajar Riansyah <fajary781@gmail.com>
2025-10-12 20:21:11 +0700

    feat: add preview good mixin

---
+++ lib/features/inventory/presentation/cubit/mixins/preview_good_mixin.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/utils/states.dart';
+
+mixin PreviewGoodMixin<S> on Cubit<S> {
+  void searchGoods(String keyword) {
+    final currentState = state;
+    if (currentState is! GoodSearchableState) return;
+
+    final allGoods = currentState.allGoods;
+    if (keyword.isEmpty) {
+      final newState = currentState.copyWithFiltered(filteredGoods: allGoods);
+      return emit(newState as S);
+    }
+
+    final lowerKeyword = keyword.toLowerCase();
+    final results = allGoods.where(
+      (good) {
+        final goodNameMatch = good.name.toLowerCase().contains(lowerKeyword);
+        final trackingNumberMatch =
+            good.receiptNumber.toLowerCase().contains(lowerKeyword);
+
+        return goodNameMatch || trackingNumberMatch;
+      },
+    ).toList();
+
+    final newState = currentState.copyWithFiltered(filteredGoods: results);
+    emit(newState as S);
+  }
+
+  void clearGoods() {
+    final currentState = state;
+    if (currentState is! GoodSearchableState) return;
+
+    final newState = currentState.copyWithFiltered(
+      allGoods: [],
+      filteredGoods: [],
+    );
+    emit(newState as S);
+  }
+}
