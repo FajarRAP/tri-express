2697d1a65dc91f51ab6356f44ff0a8dbb2984015
Fajar Riansyah <fajary781@gmail.com>
2025-10-12 15:33:32 +0700

    feat: add reusable states

---
+++ lib/core/utils/states.dart
@@ -0,0 +1,80 @@
+import 'package:equatable/equatable.dart';
+
+import '../failure/failure.dart';
+
+abstract class ReusableState<T> extends Equatable {
+  const ReusableState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class Initial<T> extends ReusableState<T> {}
+
+class Loading<T> extends ReusableState<T> {}
+
+class Loaded<T> extends ReusableState<T> {
+  const Loaded({
+    required this.data,
+    this.currentPage = 1,
+    this.hasReachedMax = false,
+    this.isPaginating = false,
+  });
+
+  final T data;
+  final int currentPage;
+  final bool hasReachedMax;
+  final bool isPaginating;
+
+  Loaded<T> copyWith({
+    T? data,
+    int? currentPage,
+    bool? hasReachedMax,
+    bool? isPaginating,
+  }) {
+    return Loaded<T>(
+      data: data ?? this.data,
+      currentPage: currentPage ?? this.currentPage,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      isPaginating: isPaginating ?? this.isPaginating,
+    );
+  }
+
+  @override
+  List<Object?> get props => [data, currentPage, hasReachedMax, isPaginating];
+}
+
+class Empty<T> extends ReusableState<T> {}
+
+class Error<T> extends ReusableState<T> {
+  const Error(this.failure);
+
+  final Failure failure;
+
+  @override
+  List<Object?> get props => [failure];
+}
+
+abstract class ActionState<T> extends ReusableState<T> {
+  const ActionState();
+}
+
+class ActionInProgress<T> extends ActionState<T> {}
+
+class ActionSuccess<T> extends ActionState<T> {
+  const ActionSuccess(this.message);
+
+  final String message;
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class ActionFailure<T> extends ActionState<T> {
+  const ActionFailure(this.failure);
+
+  final Failure failure;
+
+  @override
+  List<Object?> get props => [failure];
+}
