119b9d9a1511bc796e60041ae98817f113996885
Fajar Riansyah <fajary781@gmail.com>
2025-10-15 16:47:59 +0700

    feat (lost good entity): add copyWith method

--- lib/features/inventory/domain/entities/lost_good_entity.dart
+++ lib/features/inventory/domain/entities/lost_good_entity.dart
@@ -1,3 +1,4 @@
+import 'customer_entity.dart';
 import 'good_entity.dart';
 import 'warehouse_entity.dart';
 
@@ -19,6 +20,42 @@ class LostGoodEntity extends GoodEntity {
     required this.currentWarehouse,
     required this.issuedAt,
   });
+
+  LostGoodEntity copyWith({
+    String? id,
+    String? receiptNumber,
+    String? invoiceNumber,
+    String? name,
+    String? statusLabel,
+    String? transportMode,
+    int? status,
+    int? totalItem,
+    CustomerEntity? customer,
+    WarehouseEntity? origin,
+    WarehouseEntity? destination,
+    List<String>? allUniqueCodes,
+    List<String>? uniqueCodes,
+    WarehouseEntity? currentWarehouse,
+    DateTime? issuedAt,
+  }) {
+    return LostGoodEntity(
+      id: id ?? this.id,
+      receiptNumber: receiptNumber ?? this.receiptNumber,
+      invoiceNumber: invoiceNumber ?? this.invoiceNumber,
+      name: name ?? this.name,
+      statusLabel: statusLabel ?? this.statusLabel,
+      transportMode: transportMode ?? this.transportMode,
+      status: status ?? this.status,
+      totalItem: totalItem ?? this.totalItem,
+      customer: customer ?? this.customer,
+      origin: origin ?? this.origin,
+      destination: destination ?? this.destination,
+      allUniqueCodes: allUniqueCodes ?? this.allUniqueCodes,
+      uniqueCodes: uniqueCodes ?? this.uniqueCodes,
+      currentWarehouse: currentWarehouse ?? this.currentWarehouse,
+      issuedAt: issuedAt ?? this.issuedAt,
+    );
+  }
 
   final WarehouseEntity currentWarehouse;
   final DateTime issuedAt;
