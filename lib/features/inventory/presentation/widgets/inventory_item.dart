02cd269cf29a5b28787eb7cf7307fe8fa59f10dc
Fajar Riansyah <fajary781@gmail.com>
2025-10-15 16:58:36 +0700

    chore (inventory item): change how navigate to inventory detail

--- lib/features/inventory/presentation/widgets/inventory_item.dart
+++ lib/features/inventory/presentation/widgets/inventory_item.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
@@ -24,138 +24,150 @@ class InventoryItem extends StatelessWidget {
     const maxLength = 4;
     final length = lostGood.uniqueCodes.length;
 
-    return GestureDetector(
-      onTap: () => context.pushNamed(
-        inventoryDetailRoute,
-        extra: lostGood,
-      ),
-      child: BaseCard(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: primary50,
+    return BaseCard(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: primary50,
+                ),
+                padding: const EdgeInsets.all(10),
+                child: SvgPicture.asset(
+                  boxSvgPath,
+                  colorFilter: const ColorFilter.mode(
+                    primary,
+                    BlendMode.srcIn,
                   ),
-                  padding: const EdgeInsets.all(10),
-                  child: SvgPicture.asset(
-                    boxSvgPath,
-                    colorFilter: const ColorFilter.mode(
-                      primary,
-                      BlendMode.srcIn,
+                ),
+              ),
+              const SizedBox(width: 20),
+              Text(
+                lostGood.name,
+                style: label[medium].copyWith(
+                  color: black,
+                ),
+              ),
+              const Spacer(),
+              PopupMenuButton(
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    onTap: () => context.pushNamed(
+                      inventoryDetailRoute,
+                      extra: lostGood,
                     ),
+                    child: const Text('View Detail'),
                   ),
+                ],
+                borderRadius: BorderRadius.circular(8),
+                clipBehavior: Clip.hardEdge,
+                color: whiteTertiary,
+                iconColor: primary,
+                menuPadding: EdgeInsets.zero,
+                padding: EdgeInsets.zero,
+              ),
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            children: [
+              Expanded(
+                child: InfoTile(
+                  title: 'Nomor Invoice',
+                  value: lostGood.invoiceNumber,
                 ),
-                const SizedBox(width: 20),
+              ),
+              const SizedBox(width: 24),
+              Expanded(
+                child: InfoTile(
+                  title: 'Nomor Resi',
+                  value: lostGood.receiptNumber,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Expanded(
+                child: InfoTile(
+                  title: 'Gudang Asal',
+                  value: lostGood.origin.name,
+                ),
+              ),
+              const SizedBox(width: 24),
+              Expanded(
+                child: InfoTile(
+                  title: 'Gudang Tujuan',
+                  value: lostGood.destination.name,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            children: [
+              Expanded(
+                child: determineBadge(
+                  lostGood.status ?? -1,
+                  lostGood.statusLabel ?? '-',
+                ),
+              ),
+              const Flexible(child: SizedBox()),
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Daftar Koli',
+                style: TextStyle(
+                  color: black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              if (lostGood.uniqueCodes.length < lostGood.totalItem)
                 Text(
-                  lostGood.name,
-                  style: label[medium].copyWith(
-                    color: black,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 15),
-            Row(
-              children: [
-                Expanded(
-                  child: InfoTile(
-                    title: 'Nomor Invoice',
-                    value: lostGood.invoiceNumber,
-                  ),
-                ),
-                const SizedBox(width: 24),
-                Expanded(
-                  child: InfoTile(
-                    title: 'Nomor Resi',
-                    value: lostGood.receiptNumber,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Row(
-              children: [
-                Expanded(
-                  child: InfoTile(
-                    title: 'Gudang Asal',
-                    value: lostGood.origin.name,
-                  ),
-                ),
-                const SizedBox(width: 24),
-                Expanded(
-                  child: InfoTile(
-                    title: 'Gudang Tujuan',
-                    value: lostGood.destination.name,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 15),
-            Row(
-              children: [
-                Expanded(
-                  child: determineBadge(
-                    lostGood.status ?? -1,
-                    lostGood.statusLabel ?? '-',
-                  ),
-                ),
-                const Flexible(child: SizedBox()),
-              ],
-            ),
-            const SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Daftar Koli',
-                  style: TextStyle(
+                  '${lostGood.uniqueCodes.length}/${lostGood.totalItem} Koli',
+                  style: const TextStyle(
                     color: black,
                     fontSize: 12,
                     fontWeight: FontWeight.w700,
                   ),
-                ),
-                if (lostGood.uniqueCodes.length < lostGood.totalItem)
-                  Text(
-                    '${lostGood.uniqueCodes.length}/${lostGood.totalItem} Koli',
-                    style: const TextStyle(
-                      color: black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  )
-                else
-                  Text(
-                    '${lostGood.uniqueCodes.length} Koli',
-                    style: const TextStyle(
-                      color: black,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  )
-              ],
-            ),
-            const SizedBox(height: 22),
-            SizedBox(
-              width: double.infinity,
-              child: Wrap(
-                crossAxisAlignment: WrapCrossAlignment.start,
-                alignment: WrapAlignment.start,
-                runAlignment: WrapAlignment.start,
-                spacing: 10,
-                runSpacing: 10,
-                children: List.generate(
-                  length < maxLength ? length : maxLength,
-                  (index) => _UniqueCodeBadge(
-                    uniqueCode: lostGood.uniqueCodes[index],
+                )
+              else
+                Text(
+                  '${lostGood.uniqueCodes.length} Koli',
+                  style: const TextStyle(
+                    color: black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
                   ),
+                )
+            ],
+          ),
+          const SizedBox(height: 22),
+          SizedBox(
+            width: double.infinity,
+            child: Wrap(
+              crossAxisAlignment: WrapCrossAlignment.start,
+              alignment: WrapAlignment.start,
+              runAlignment: WrapAlignment.start,
+              spacing: 10,
+              runSpacing: 10,
+              children: List.generate(
+                length < maxLength ? length : maxLength,
+                (index) => _UniqueCodeBadge(
+                  uniqueCode: lostGood.uniqueCodes[index],
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
